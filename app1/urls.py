951bd75881063d7ae3ef0230a8ff59f1ed40db1f
Mohammad-Safaie-1999 <mohammadsafaie1999@gmail.com>
2024-01-21 18:22:06 +0330

    final changes

--- app1/urls.py
+++ app1/urls.py
@@ -2,5 +2,9 @@ from django.urls import path,include
 from app1.views import *
 
 urlpatterns = [
-    path('',index_home)
+    path('',index_home,name='home'),
+    path('abilities/',index_abilities,name='abilities'),
+    path('contact/',index_contact,name='contact'),
+    path('education/',index_education,name='education'),
+    path('languages/',index_languages,name='languages')
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
 def index_home(request):
-    return render(request,'index.html')
+    return render(request,'homepage.html')
+
+def index_abilities(request):
+    return render(request,'abilitiespage.html')
+
+def index_contact(request):
+    return render(request,'contactpage.html')
+
+def index_education(request):
+    return render(request,'educationpage.html')
+
+def index_languages(request):
+    return render(request,'languagespage.html')
