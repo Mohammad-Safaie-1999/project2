a6f08f53766bfda6237e8c213ee5f6869a3211c5
Mohammad-Safaie-1999 <mohammadsafaie1999@gmail.com>
2024-01-21 14:31:21 +0330

    adding templates and static files

---
+++ app1/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path,include
+from app1.views import *
+
+urlpatterns = [
+    path('',index_home)
+]
--- site1/settings.py
+++ site1/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=[BASE_DIR/'static']
+STATIC_ROOT=BASE_DIR/'static'
 MEDIA_URL = 'media/'
-MEDIA_ROOT=[BASE_DIR/'media']
+MEDIA_ROOT=BASE_DIR/'media'
 STATICFILES_DIRS=[BASE_DIR/'statics']
 
 
